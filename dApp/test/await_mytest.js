7533c67a7c034aee6d41208c3d4aced9d1110e94
Remo <andreoliremo@gmail.com>
2019-06-23 13:33:50 +0200

    lite-server seems to work, now it's time to set up the page

--- dApp/test/await_mytest.js
+++ dApp/test/await_mytest.js
@@ -1,10 +1,11 @@
 const englishAuction = artifacts.require("englishAuction");
 
-contract("englishAuction", function(accounts) => {
-    it("test the correctness of the functions", async function() => {
-    	const instance = await englishAuction.deployed();
-	const result = await instance.getAuctionLength();
-	
-	console.log(result);
+contract("englishAuction", accounts => {
+    it("test the correctness of the functions", () => {
+		const instance = await englishAuction.deployed();
+		const result = await instance.getAuctionLength();
+
+
+		console.log(result);
     });
 });
