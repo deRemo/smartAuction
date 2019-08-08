3c1e0b3e22b74f603bcda267a2d5764a07b82d6d
Remo <andreoliremo@gmail.com>
2019-08-08 22:47:41 +0200

    refreshed gitignore, removed truffle files from the old version of the dapp (interference with the new one)

--- dApp_old/migrations/2_smartAuctionFactory_migration.js
+++
@@ -1,6 +0,0 @@
-//load the englishAuction.json from the build folder
-const factory = artifacts.require("smartAuctionFactory");
-
-module.exports = function(deployer){
-	deployer.deploy(factory);
-};
